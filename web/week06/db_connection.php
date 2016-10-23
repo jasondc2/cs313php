4e1abad1c4e2f4f9694b19323a5f97257aa49a5a
Jason Christensen <jasondchristensen@outlook.com>
2016-10-22 21:59:01 -0600

    correction to db query

--- web/week06/db_connection.php
+++ web/week06/db_connection.php
@@ -4,7 +4,7 @@
 
     if (empty($dbUrl)) {
      // example localhost configuration URL with postgres username and a database called cs313db
-        require('../../../cs313/model/localDB.php');
+        $dbUrl = "postgres://postgres:password@localhost:5432/cs313db";
     }
 
     $dbopts = parse_url($dbUrl);
